5e39890daead8469418a575e9ab090a56eaf2307
ieva <ievabroberg@gmail.com>
2025-09-07 15:21:20 +0300

    edit db url

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,12 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
-from dotenv import load_dotenv
-
-# Load variables from .env
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
